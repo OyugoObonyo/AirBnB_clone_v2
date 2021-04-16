1430ee7eba68f6c1164fee594bf0b95cc34259fe
OyugoObonyo <nyikal1999@gmail.com>
2021-04-16 15:53:07 +0300

    Set up web servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 #Bash script that sets up web server for deployment
 
+#install nginx web server 
 apt-get update
-apt install nginx
+apt-get install -y nginx
+
+#create necessary docs and dirs
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Holberton School" >> /data/web_static/releases/test/index.html
+
+#create symbolic link as specified
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#set ubuntu as group and user
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
+#tweak nginx configurations
 printf %s "server {
         listen 80 default_server;
         listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
         root /var/www/html;
         index index.html index.htm;
-        add_header X-Served-By $HOSTNAME;
         
         location /hbnb_static {
             alias /data/web_static/current;
@@ -24,4 +32,5 @@ printf %s "server {
         }
 }" > /etc/nginx/sites-available/default
 
+#Restart nginx for changes to take effect
 service nginx restart
