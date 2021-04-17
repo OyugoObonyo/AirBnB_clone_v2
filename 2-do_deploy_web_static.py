ac43e01b96a44d42c0f4db818ca0edd90d9cb531
OyugoObonyo <nyikal1999@gmail.com>
2021-04-17 19:04:23 +0300

    Deploy archive to web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,65 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-from fabric.api import env, run, sudo
-from datetime import datetime
-import os.path
+# Distributes archived pack to both web servers
 
-env.user = 'ubuntu'
-env.hosts = ['34.73.50.31', '34.73.156.72']
+import os.path
+from fabric.api import env, put, run
+
+env.user = "ubuntu"
+env.hosts = ["34.73.50.31", "34.73.156.72"]
 
 
 def do_deploy(archive_path):
-    """Distributes archives to web servers
+    """Distributes an archive to a web server.
        Returns True if successful and false if not
     """
-    try:
-        if os.path.isfile(archive_path) is False:
-            return False
-        archFile = archive_path.split('/')[-1]
-        archStamp = archFile.split('.')[0]
+    if os.path.isfile(archive_path) is False:
+        return False
+    fullFile = archive_path.split("/")[-1]
+    folder = fullFile.split(".")[0]
 
-        # Upload archive to /tmp/ directory
-        put(archive_path, '/tmp/{}'.format(archFile))
-
-        # Uncompress archive to /data/web_static/current/ directory
-        run('mkdir -p /data/web_static/releases/{}/'.format(archStamp))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archFile, archStamp))
-
-        # Delete archive from web server
-        run('rm /tmp/{}'.format(archFile))
-
-        # Delete symbolic link from web server
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'
-            .format(archStamp, archStamp))
-        run('rm -rf /data/web_static/releases/{}/web_static/'
-            .format(archStamp))
-
-        # Create new symbolic link on web server linked to new code version
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(archStamp))
-    except:
+    # Uploads archive to /tmp/ directory
+    if put(archive_path, "/tmp/{}".format(fullFile)).failed is True:
+        print("Uploading archive to /tmp/ failed")
         return False
 
+    # Uncompress archive to /data/web_static/current/ directory
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(folder)).failed is True:
+        print("Deleting folder with archive(if already exists) failed")
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(folder)).failed is True:
+        print("Creating new archive folder failed")
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(fullFile, folder)).failed is True:
+        print("Uncompressing archive to failed")
+        return False
+
+    # Deletes archive from web server
+    if run("rm /tmp/{}".format(fullFile)).failed is True:
+        print("Deleting archive from /tmp/ directory dailed")
+        return False
+
+    # Deletes symbolic link from web server
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".
+           format(folder, folder)).failed is True:
+        print("Moving content to archive folder before deletion failed")
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(folder)).failed is True:
+        print("Deleting web_static folder failed")
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        print("Deleting 'current' folder failed")
+        return False
+
+    # Create new symbolic link on web server linked to new code version
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(folder)).failed is True:
+        print("Creating new symbolic link to new code version failed")
+        return False
+
+    print("New version deployed!")
     return True
