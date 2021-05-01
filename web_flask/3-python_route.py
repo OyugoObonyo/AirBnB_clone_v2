5407ddd2e7c95176db07d6259e16a0fa60c9bfa6
OyugoObonyo <nyikal1999@gmail.com>
2021-05-01 20:03:30 +0300

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>')
+def displaytext(text):
+    """Display C followed by variable entered by user"""
+    return "C" + " " + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<string:text>')
+def python_is_cool(text="is cool"):
+    """Display text based on input"""
+    return "Python" + " " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
